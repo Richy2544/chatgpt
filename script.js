28953980cddaddcb645b10787c44914b4bdeabe4
Richy2544 <riccardogiuseppesicali@gmail.com>
2024-12-31 17:58:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-import * as THREE from './node_modules/three/build/three.module.js';
-import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls.js';
-import { GLTFLoader } from './node_modules/three/examples/jsm/loaders/GLTFLoader.js';
-
+import * as THREE from './libs/three.module.js';
+import { OrbitControls } from './libs/OrbitControls.js';
+import { GLTFLoader } from './libs/GLTFLoader.js';
 
 // Debug: Messaggi iniziali
 console.log('Script avviato');
